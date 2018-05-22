3a151b3e677948fdf01c1f09364138ac1608baf4
Michael Kang <michaelkang85@gmail.com>
2018-05-22 00:07:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,40 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    "Hello World"
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @post = Post.create(params)
+    erb :index
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+     @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do  #updates a post
+  @post = Post.find_by_id(params[:id])
+  @post.name = params[:name]
+  @post.content = params[:content]
+  @post.save
+  erb :show
+ end
+
+
 end
